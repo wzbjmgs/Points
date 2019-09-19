0ac55f9ef93366394a3c08a670587c60415ff7b2
Jayden Wang <Jayden@backbase.com>
2019-09-19 19:40:54 -0400

    Application Skeleton.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Points',
+    version='1.0',
+    packages=['test', 'points'],
+    url='',
+    license='',
+    author='Jaden',
+    author_email='',
+    description='Points MARS ROVERS Assigment'
+)
